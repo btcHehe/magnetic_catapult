f8adf63f17e03dcea31c6f7229f4749238ec5542
btc <btc121314@gmail.com>
2024-04-13 18:23:10 +0200

    small corrections to angle calculations

--- src/IMU9DoF.cpp
+++ src/IMU9DoF.cpp
@@ -12,12 +12,23 @@ angle_t IMU9DoF::getPitch() {
     this->getGData(g);
     float G = sqrt(g[0]*g[0] + g[1]*g[1] + g[2]*g[2]);
     angle_t alfa1 = asin(g[0]/G);
-    angle_t alfa2 = acos(sqrt(g[1]*g[1] + g[2]*g[2])/G);
-    angle_t alfa3 = (alfa1 <= MATH_PI/4) ? alfa1 : alfa2;
+    angle_t alfa2 = acos(sqrt(g[1]*g[1]/(G*G) + g[2]*g[2]/(G*G)));
+    angle_t alfa3 = 0;
+    if(alfa1 >= -MATH_PI/4 && alfa1 <= MATH_PI/4) {
+        alfa3 = alfa1;
+    } else { 
+        alfa3 = alfa2;
+    }
     angle_t w1 = cos(alfa3)*cos(alfa3);
     angle_t w2 = sin(alfa3)*sin(alfa3);
     angle_t alfa4 = w1*alfa1 + w2*alfa2;
-    return alfa4;
+    if(g[2] > 0 ) {
+        return alfa4;
+    } else if(g[2] < 0 && alfa4 > 0) {
+        return MATH_PI-alfa4;
+    } else if(g[2] < 0 && alfa4 < 0) {
+        return -MATH_PI-alfa4;
+    }
 }
 
 angle_t IMU9DoF::getRoll() {
@@ -26,11 +37,22 @@ angle_t IMU9DoF::getRoll() {
     float G = sqrt(g[0]*g[0] + g[1]*g[1] + g[2]*g[2]);
     angle_t beta1 = asin(g[1]/G);
     angle_t beta2 = acos(sqrt(g[0]*g[0] + g[2]*g[2])/G);
-    angle_t beta3 = (beta1 <= MATH_PI/4) ? beta1 : beta2;
+    angle_t beta3 = 0;
+    if(beta1 >= -MATH_PI/4 && beta1 <= MATH_PI/4) {
+        beta3 = beta1;
+    } else { 
+        beta3 = beta2;
+    }
     angle_t w1 = cos(beta3)*cos(beta3);
     angle_t w2 = sin(beta3)*sin(beta3);
     angle_t beta4 = w1*beta1 + w2*beta2;
-    return beta4;
+    if(g[2] > 0 ) {
+        return beta4;
+    } else if(g[2] < 0 && beta4 > 0) {
+        return MATH_PI-beta4;
+    } else if(g[2] < 0 && beta4 < 0) {
+        return -MATH_PI-beta4;
+    }
 }
 
 angle_t IMU9DoF::getYaw() {
